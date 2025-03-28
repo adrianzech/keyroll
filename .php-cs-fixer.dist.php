96af202caedbbcd894e0ce686c641997773b6abd
Adrian Zech <adrian@zech.co>
2025-03-28 14:08:26 +0100

    Added php-cs-fixer, phpmd and phpstan
    Signed-off-by: Adrian Zech <adrian@zech.co>

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = new PhpCsFixer\Finder()
+    ->in(__DIR__)
+    ->exclude('var')
+    ->exclude('vendor')
+    ->exclude('public/build')
+    ->exclude('assets/vendor')
+    ->notPath('src/Kernel.php')
+    ->notPath('public/index.php')
+    ->name('*.php');
+
+return new PhpCsFixer\Config()
+    ->setRules([
+        '@Symfony'             => true,
+        'declare_strict_types' => true,
+        'yoda_style'           => false,
+        'concat_space'         => ['spacing' => 'one'],
+    ])
+    ->setFinder($finder)
+    ->setRiskyAllowed(true)
+    ->setUsingCache(true);
