ea5dd32494f8134e17755ba401846cce50d8ff16
Adrian Zech <adrian@zech.co>
2025-05-15 22:01:41 +0200

    Added missing use statement

--- src/Form/HostType.php
+++ src/Form/HostType.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Form;
 
 use App\Entity\Host;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
