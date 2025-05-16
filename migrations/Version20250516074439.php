feab6fd199f33b11735bda72a9d38edbe95bd69b
Adrian Zech <adrian@zech.co>
2025-05-16 10:38:50 +0200

    Updated migrations

--- migrations/Version20250516074439.php
+++ migrations/Version20250516074439.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250430140420 extends AbstractMigration
+final class Version20250516074439 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -24,16 +24,31 @@ final class Version20250430140420 extends AbstractMigration
             CREATE TABLE `category` (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', updated_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', UNIQUE INDEX UNIQ_64C19C15E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
+            CREATE TABLE `host` (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, hostname VARCHAR(255) NOT NULL, port INT NOT NULL, username VARCHAR(255) NOT NULL, connection_status VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', updated_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', UNIQUE INDEX UNIQ_CF2713FD5E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
             CREATE TABLE host_category (host_id INT NOT NULL, category_id INT NOT NULL, INDEX IDX_57A5E5D71FB8D185 (host_id), INDEX IDX_57A5E5D712469DE2 (category_id), PRIMARY KEY(host_id, category_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
+            CREATE TABLE `ssh_key` (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, name VARCHAR(255) NOT NULL, public_key LONGTEXT NOT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', updated_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', INDEX IDX_82A73B64A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT '(DC2Type:json)', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D6495E237E06 (name), UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
             CREATE TABLE user_category (user_id INT NOT NULL, category_id INT NOT NULL, INDEX IDX_E6C1FDC1A76ED395 (user_id), INDEX IDX_E6C1FDC112469DE2 (category_id), PRIMARY KEY(user_id, category_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', available_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', delivered_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
             ALTER TABLE host_category ADD CONSTRAINT FK_57A5E5D71FB8D185 FOREIGN KEY (host_id) REFERENCES `host` (id) ON DELETE CASCADE
         SQL);
         $this->addSql(<<<'SQL'
             ALTER TABLE host_category ADD CONSTRAINT FK_57A5E5D712469DE2 FOREIGN KEY (category_id) REFERENCES `category` (id) ON DELETE CASCADE
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE `ssh_key` ADD CONSTRAINT FK_82A73B64A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id)
         SQL);
         $this->addSql(<<<'SQL'
             ALTER TABLE user_category ADD CONSTRAINT FK_E6C1FDC1A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE CASCADE
@@ -53,6 +68,9 @@ final class Version20250430140420 extends AbstractMigration
             ALTER TABLE host_category DROP FOREIGN KEY FK_57A5E5D712469DE2
         SQL);
         $this->addSql(<<<'SQL'
+            ALTER TABLE `ssh_key` DROP FOREIGN KEY FK_82A73B64A76ED395
+        SQL);
+        $this->addSql(<<<'SQL'
             ALTER TABLE user_category DROP FOREIGN KEY FK_E6C1FDC1A76ED395
         SQL);
         $this->addSql(<<<'SQL'
@@ -62,10 +80,22 @@ final class Version20250430140420 extends AbstractMigration
             DROP TABLE `category`
         SQL);
         $this->addSql(<<<'SQL'
+            DROP TABLE `host`
+        SQL);
+        $this->addSql(<<<'SQL'
             DROP TABLE host_category
         SQL);
         $this->addSql(<<<'SQL'
+            DROP TABLE `ssh_key`
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE `user`
+        SQL);
+        $this->addSql(<<<'SQL'
             DROP TABLE user_category
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE messenger_messages
         SQL);
     }
 }
