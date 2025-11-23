635b3481e2e6e1ee158c838a27bb5873b9310cd2
Adrian Zech <adrian@zech.co>
2025-11-23 10:56:51 +0100

    Improved ssh key filtering and searching

--- src/DataTable/SSHKeyDataTableType.php
+++ src/DataTable/SSHKeyDataTableType.php
@@ -5,17 +5,22 @@ declare(strict_types=1);
 namespace App\DataTable;
 
 use App\Entity\SSHKey;
+use App\Entity\User;
 use Kreyu\Bundle\DataTableBundle\Action\Type\ButtonActionType;
 use Kreyu\Bundle\DataTableBundle\Action\Type\FormActionType;
+use Kreyu\Bundle\DataTableBundle\Bridge\Doctrine\Orm\Filter\Type\DateFilterType;
 use Kreyu\Bundle\DataTableBundle\Bridge\Doctrine\Orm\Filter\Type\StringFilterType;
 use Kreyu\Bundle\DataTableBundle\Bridge\OpenSpout\Exporter\Type\CsvExporterType;
 use Kreyu\Bundle\DataTableBundle\Bridge\OpenSpout\Exporter\Type\OdsExporterType;
 use Kreyu\Bundle\DataTableBundle\Bridge\OpenSpout\Exporter\Type\XlsxExporterType;
 use Kreyu\Bundle\DataTableBundle\Column\Type\TextColumnType;
 use Kreyu\Bundle\DataTableBundle\DataTableBuilderInterface;
+use Kreyu\Bundle\DataTableBundle\Filter\FilterData;
+use Kreyu\Bundle\DataTableBundle\Filter\Type\CallbackFilterType;
 use Kreyu\Bundle\DataTableBundle\Pagination\PaginationData;
 use Kreyu\Bundle\DataTableBundle\Query\ProxyQueryInterface;
 use Kreyu\Bundle\DataTableBundle\Type\AbstractDataTableType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
@@ -59,11 +64,43 @@ class SSHKeyDataTableType extends AbstractDataTableType
             ->addFilter('name', StringFilterType::class, [
                 'label' => 'entity.ssh_key.label.name',
             ])
+            ->addFilter('user', CallbackFilterType::class, [
+                'label' => 'entity.ssh_key.label.user',
+                'form_type' => EntityType::class,
+                'form_options' => [
+                    'class' => User::class,
+                    'choice_label' => 'name',
+                    'required' => false,
+                ],
+                'active_filter_formatter' => static fn (FilterData $data): string => $data->getValue() instanceof User
+                    ? $data->getValue()->getName()
+                    : '',
+                'callback' => function (ProxyQueryInterface $query, FilterData $data): void {
+                    if (!$data->hasValue()) {
+                        return;
+                    }
+
+                    $user = $data->getValue();
+
+                    if (!$user instanceof User) {
+                        return;
+                    }
+
+                    /* @noinspection PhpUndefinedMethodInspection */
+                    $query
+                        ->andWhere('ssh_key.user = :user')
+                        ->setParameter('user', $user);
+                },
+            ])
+            ->addFilter('createdAt', DateFilterType::class, [
+                'label' => 'common.label.created_at',
+                'operator_selectable' => true,
+            ])
             ->setSearchHandler(function (ProxyQueryInterface $query, string $search): void {
                 /* @noinspection PhpUndefinedMethodInspection */
                 $query
                     ->leftJoin('ssh_key.user', 'user')
-                    ->andWhere('ssh_key.name LIKE :search OR user.name LIKE :search')
+                    ->andWhere('ssh_key.name LIKE :search OR user.name LIKE :search OR user.email LIKE :search')
                     ->setParameter('search', '%' . $search . '%');
             })
             ->addExporter('csv', CsvExporterType::class, [
